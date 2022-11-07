0d135940f59af0bf1e808a0695b32ee6ffd3d299
Daniel Lemky <daniel@quantumfirelabs.com>
2022-11-06 19:54:37 -0600

    Deal cards

--- main.js
+++ main.js
@@ -9,3 +9,10 @@ let pawPatrolCards = [
 	'chickaletta',
 	'liberty'
 ]
+
+function dealCards() {
+	cards = pawPatrolCards
+	cards = cards.concat(pawPatrolCards)
+	cards = cards.sort(() => Math.random() - 0.5);
+}
+dealCards()
